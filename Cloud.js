be94f03e13840dcbb110667e1134cb1019acaac2
konstantin <motorniy.freshcode@gmail.com>
2017-01-13 16:52:38 +0200

    tweaks

--- Cloud.js
+++ Cloud.js
@@ -1,29 +1,27 @@
-function Cloud(z){
-	var range = 300;
-	this.x = Math.ceil(Math.random()*range)-range/2;
-	this.z = z;
-	this.y = 0
+function Cloud(z) {
+    var range = settings.dispersion || 300;
+    this.x = Math.ceil(Math.random() * range) - range / 2;
+    this.z = z;
+    this.y = 0;
 
-	this.xV = this.x > 0 ? -0.5 : 0.5;
-	this.yV = 0;
-	this.zV = 0;
+    this.xV = Math.random() - 0.5 < 0 ? -0.5 : 0.5;
+    this.yV = 0;
+    this.zV = 0;
 }
 
-Cloud.prototype.update = function(){
-	this.x += this.xV;
+Cloud.prototype.update = function () {
+    this.x += this.xV;
 
-	if(this.x > 400 || this.x < -400){
-		this.xV = -this.xV
-	}
+    return this;
+};
 
-	return this;
-}
+Cloud.prototype.toString = function () {
+    var styleObj = {
+        transform: 'translate3d(' + this.x + 'px, ' + this.y + 'px, -' + this.z + 'px)',
+        filter: 'blur(' + Math.ceil(this.z / 2000) + 'px)'
+    };
 
-Cloud.prototype.toString = function(){
-	var styleStr = style({
-		transform :  'translate3d(' + this.x + 'px, '+ this.y + 'px, -' + this.z + 'px)',
-		filter : 'blur('+ Math.ceil(this.z / 1000) +'px)'
-	})
+    styleObj['-webkit-filter'] = styleObj['-moz-filter'] = styleObj.filter;
 
-	return '<div class="step" style="'+ styleStr+'"></div>';
-}
+    return '<div class="cloud" style="' + style(styleObj) + '"></div>';
+};
--- app.js
+++ app.js
@@ -1,103 +1,122 @@
-var vars = {
-	guidelines : false,
-	_fps : 25
-}	
+var settings = {
+    cloudDeltaZ: 100,
+    dispersion: 2000,
+    clouds: 20,
+    guidelines: false,
+    _fps: 25
+};
 
-Object.defineProperty(vars, 'fps', {
-  	enumerable: true,
-  	configurable: true,
-	get: function(){
-		return this._fps;
-	},
-	set : function(val){
-		this._fps = val;
-		start();
-	}
-})
+Object.defineProperty(settings, 'fps', {
+    enumerable: true,
+    configurable: true,
+    get: function () {
+        return this._fps;
+    },
+    set: function (val) {
+        this._fps = val;
+        start();
+    }
+});
 
 
-function renderGuidlines(){
-	if(vars.guidelines){
-		$('.container').append('<div class="guideline gtl"></div><div class="guideline gtr"></div><div class="guideline gbl"></div><div class="guideline gbr"></div>');
-	}
+function renderGuidlines() {
+    if (settings.guidelines) {
+        $('.container').append(
+            '<div class="guideline gt"></div>'
+            +'<div class="guideline back"></div>'
+            +'<div class="guideline gb"></div>');
+    }
 }
 
-function renderClouds(){
-	var result = ''
+function renderClouds() {
+    var result = '';
 
-	for(var i= 0; i<clouds.length; i+=1){
-		result += clouds[i].update();
-	}
+    for (var i = 0; i < clouds.length; i += 1) {
+        result += clouds[i].update();
+    }
 
-	$('.container').append(result);
+    $('.container').append(result);
 }
 
-function renderSettings(){
-	var keys = Object.keys(vars);
+function renderSettings() {
+    var keys = Object.keys(settings);
 
-	for(var i = 0; i < keys.length; i++){
-		if(keys[i][0] == "_"){
-			continue;
-		}
-		
-		var type = 'text';
+    for (var i = 0; i < keys.length; i++) {
+        if (keys[i][0] == "_") {
+            continue;
+        }
 
-		switch(typeof vars[keys[i]]){
-			case 'boolean': type = 'checkbox'; break;
-			case 'number': type = 'number'; break;
-			case 'string':
-			default: type = 'text';
-		}
+        var type = 'text';
 
-		$('.settings')
-		.append('<label for=var-'+keys[i]+'">'+keys[i]+'</label>')
-		.append('<input class="input-'+type+'" value="'+vars[keys[i]]+'" id="'+keys[i]+'" type="'+type+'"/>')
-		.append('<br/>');
+        switch (typeof settings[keys[i]]) {
+            case 'boolean':
+                type = 'checkbox';
+                break;
+            case 'number':
+                type = 'number';
+                break;
+            case 'string':
+            default:
+                type = 'text';
+        }
 
-	}
-	$('.settings').append('<button onclick="stop()">Stop</button><button onclick="start()">Start</button>');
+        $('.settings')
+            .append('<label for=var-' + keys[i] + '">' + keys[i] + '</label>')
+            .append('<input class="input-' + type + '" value="' + settings[keys[i]] + '" id="' + keys[i] + '" type="' + type + '"/>')
+            .append('<br/>');
 
-	$('.settings .input-text').on('change', function(event){
-		vars[this.id] = this.value;
-	})
+    }
+    $('.settings').append(
+        '<button onclick="setup()">Reset</button>'
+        +'<button onclick="stop()">Stop</button>'
+        +'<button onclick="start()">Start</button>');
 
-	$('.settings .input-number').on('change', function(event){
-		vars[this.id] = this.value;
-	})
+    $('.settings .input-text').on('change', function (event) {
+        settings[this.id] = this.value;
+    });
 
-	$('.settings .input-checkbox').on('change', function(event){
-		vars[this.id] = this.checked;
-	})
+    $('.settings .input-number').on('change', function (event) {
+        settings[this.id] = this.value;
+    });
+
+    $('.settings .input-checkbox').on('change', function (event) {
+        settings[this.id] = this.checked;
+    })
 }
 
-function update(){
-	$('.container').html('');
+function update() {
+    $('.container').html('');
 
-	renderGuidlines();
-	renderClouds();
+    renderGuidlines();
+    renderClouds();
 }
 
-function stop(){
-	window.clearInterval(vars.interval);
+function stop() {
+    window.clearInterval(settings.interval);
 }
 
-function start(){
-	stop();
-	vars.interval = setInterval(update, 1000/vars.fps)
+function start() {
+    stop();
+    settings.interval = setInterval(update, 1000 / settings.fps)
 }
 
 
 var clouds = [];
 
-function fillClouds(number){
-	clouds.length = 0;
-	for(var i = 0; i < number; i++){
-		clouds.push(new Cloud(i*500));
-	}
+function fillClouds(number) {
+    clouds.length = 0;
+    for (var i = 0; i < number; i++) {
+        clouds.push(new Cloud(i * settings.cloudDeltaZ));
+    }
 }
 
-$(document).ready(function(){
-	renderSettings();
-	fillClouds(10);
-	start();
+function setup() {
+    stop();
+    fillClouds(settings.clouds);
+    start();
+}
+
+$(document).ready(function () {
+    renderSettings();
+    setup();
 });
